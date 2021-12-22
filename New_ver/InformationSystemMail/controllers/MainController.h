e8ffc71ea1eb30fe83a50179717a74eae30d78af
Vasiliy Prokhorov <56435313+Stinger1000@users.noreply.github.com>
2021-12-22 16:30:10 +0300

    Fix: gitignore

--- New_ver/InformationSystemMail/controllers/MainController.h
+++ New_ver/InformationSystemMail/controllers/MainController.h
@@ -4,6 +4,11 @@
 #include <QDebug>
 #include <QObject>
 #include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QSqlRecord>
+#include <QSqlTableModel>
 
 #include "models/BaseModel.h"
 
@@ -15,21 +20,34 @@ public:
 public slots:
     //actions //--FROM UI--//
     //    void CreateNewDep();
-    //    void AddAdminToDep();
-    //    void RemoveAdminFromDep();
-    //    void RemoveDep();
-    //    void AddTelecomOperPostman();
-    //    void RemoveTelecomOperPostman();
-    //    void AddForwardPoints();
-    //    void RemoveForwardPoints();
-    //    void IssueShip();
-    //    void SendShipToNextPoint();
-    //    void ViewListShipInDep();
-    //    void ViewShipInfo();
-    //    void AcceptShip();
-    //    void DeliverNotice();
-    //    void AcceptShipFromDep();
+    void CreateUser(const User& user);
+    void FindUser(const User& user);
+    void DeleteUser(const User& user);
+
+    void CreatePost(const Post& post);
+    void FindPost(const Post& post);
+    void DeletePost(const Post& post);
+
+    void CreateDeparture(const Departure& dep);
+    void FindDeparture(const Departure& dep);
+
+    void ActionDep(ActionDeparture action, const QList<Departure>& dep);
+
+    void SendDepNext(int32_t id_dep, const QList<Departure>& dep);
+
+    void ViewDeparture(enum TypeViewDep typeView);
+
+    void GetNextDep();
+
+    void GetFullInfoDeparture(int32_t id_dep);
+
+    void UpdatePostForForward(const QString& country, const QString& region, const QString& local);
+
+    void AddForwardPost(int32_t post);
+    void DeleteForwardPost(const QList<int32_t>& list);
+
     void LogIn(const User& user);
+    void GetPostList();
     //--
 
     void SetHost(const QString host);
@@ -37,11 +55,26 @@ public slots:
 signals:
     //ANSWER //--TO UI--//
     void StatusLogIn(const User& user, bool status);
+    void PostList(const QList<Post>& list);
+    void FindUserList(const QList<User>& users);
+    void FindPostList(const QList<Post>& posts);
+
+    void SelectTable(QSqlTableModel* model);
+
+    void ViewPostForForward(const QList<Post>& list);
+
+    void ViewForwardPost(const QList<Post>& list);
+
+    void AdditInfoForDeparture(const QList<AdditInfoDeparture>& info);
+
+    void Error(ErrorType error, const QString& text);
 
 private:
     QSqlDatabase m_db;
     bool         m_status_connect { false };
     QString      m_current_host { "localhost" };
+
+    QSqlTableModel* m_model { nullptr };
 };
 
 #endif // MAINCONTROLLER_H
